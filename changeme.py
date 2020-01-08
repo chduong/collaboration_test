608e1affbe96d30d0d17265fd9cd80fa62396ddf
Chinh <chduong@users.noreply.github.com>
2020-01-08 01:24:02 -0500

    Changed Branch 1, Rebased Master to Branch 1, then Modified Master.

--- changeme.py
+++ changeme.py
@@ -3,3 +3,5 @@ print("Rashedul was here. :)")
 print('Chinh pulled 2 versions and deleted one of them (after disconnecting from version control in Pycharm/git associations), then added this line to the remaining file. Then committed and pushed.')
 
 print('Branched Once. Then committed and pushed.')
+
+print('Modified Branch 1, then rebased master to branch 1 and modified master. Then committed and pushed master.')
