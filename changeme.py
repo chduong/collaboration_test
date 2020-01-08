e4ae6a9e2a890f9d3a6df981b14da91de789889f
Chinh <chduong@users.noreply.github.com>
2020-01-08 01:05:04 -0500

    Pulled twice, deleted one file, modified the other.

--- changeme.py
+++ changeme.py
@@ -1,2 +1,3 @@
 print("Person 1 changed this file.")
 print("Rashedul was here. :)")
+print('Chinh pulled 2 versions and deleted one of them (after disconnecting from version control in Pycharm/git associations), then added this line to the remaining file. Then committed and pushed.')
